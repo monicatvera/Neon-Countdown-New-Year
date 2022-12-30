bee046bd7ed4fd159b13093548fb5919645988f4
Mónica Ilenia Tardón Vera <monitvera@gmail.com>
2022-12-30 19:59:06 +0000

    🎉intial proyect

---
+++ README.md
@@ -0,0 +1,2 @@
+# Neon-Countdown-New-Year
+A neon web page with fireworks in the background and a new year countdown. Written in html, css, and javascript
---
+++ timer.js
@@ -0,0 +1,20 @@
+const countDownDate = new Date("Jan 1, 2023 0:0:0").getTime();
+
+const x = setInterval(function () {
+  let now = new Date().getTime();
+
+  let distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "2023";
+  }
+}, 1000);
